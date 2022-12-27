aaad021c87133e38f74588efd595470edc9136a3
SanjaySiddharth <mjsanjaysiddharth1999@gmail.com>
2022-12-27 13:31:53 +0530

    Initial Commit

---
+++ EmojiArt/EmojiArtApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiArtApp.swift
+//  EmojiArt
+//
+//  Created by Sanjay Siddharth on 27/12/22.
+//
+
+import SwiftUI
+
+@main
+struct EmojiArtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
